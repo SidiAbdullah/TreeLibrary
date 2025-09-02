f13030b7984b5ba7f6e698ea71a1aaf25383fb42
Sidi <Sidi.com>
2025-09-02 10:24:27 +0000

    add

--- Program.cs
+++ Program.cs
@@ -1,66 +1,183 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace GeneralTreeExample
+namespace BinaryTreeImplementation
 {
-    public class TreeNode<T>
+    public class BinaryTreeNode<T>
     {
         public T Value { get; set; }
-        public List<TreeNode<T>> Children { get; set; }
+        public BinaryTreeNode<T> Left { get; set; }
+        public BinaryTreeNode<T> Right { get; set; }
 
-        public TreeNode(T value)
+        public BinaryTreeNode(T value)
         {
-            this.Value = value;
-            Children = new List<TreeNode<T>>();
+            Value = value;
+            Left = null;
+            Right = null;
         }
-
-        public void AddChild(TreeNode<T> child)
-        {
-            Children.Add(child);
-        }
-
     }
 
-    public class Tree<T>
+    public class BinaryTree<T>
     {
-        public TreeNode<T> Root { get; private set; }
+        public BinaryTreeNode<T> Root { get; private set; }
 
-        public Tree(T rootValue)
+
+        public BinaryTree()
         {
-            Root = new TreeNode<T>(rootValue);
-        }
-        private static void PrintTree(TreeNode<T> node, string indent = " ")
-        {
-            Console.WriteLine(indent + node.Value);
-            foreach (var child in node.Children)
-            {
-                PrintTree(child, indent + "  ");
-            }
-        }
-        public void PrintTree(string indent = " ")
-        {
-            Console.WriteLine(indent + this.Root.Value);
-            foreach (var child in this.Root.Children)
-            {
-                PrintTree(child, indent + "  ");
-            }
+            Root = null;
         }
 
-        private TreeNode<T> Find(TreeNode<T> root, T value)
+        public void Insert(T value)
         {
-            if (root.Value.Equals(value)) return root;
-
-            foreach (var child in root.Children)
+            var newNode = new BinaryTreeNode<T>(value);
+            if (Root == null)
             {
-                var found = Find(child, value);
-                if (found != null) return found;
+                Root = newNode;
+                return;
             }
 
-            return null;
+            Queue<BinaryTreeNode<T>> queue = new Queue<BinaryTreeNode<T>>();
+            queue.Enqueue(Root);
+
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+
+
+                if (current.Left == null)
+                {
+                    current.Left = newNode;
+                    break;
+                }
+                else
+                {
+                    queue.Enqueue(current.Left);
+                }
+
+
+                if (current.Right == null)
+                {
+                    current.Right = newNode;
+                    break;
+                }
+                else
+                {
+                    queue.Enqueue(current.Right);
+                }
+            }
         }
-        public TreeNode<T> Find(T value)
+
+        // Print the tree visually
+        public void PrintTree()
         {
-            return Find(this.Root, value);
+            PrintTree(Root, 0);
+        }
+        private void PrintTree(BinaryTreeNode<T> root, int space)
+        {
+            int COUNT = 10;  // Distance between levels
+            if (root == null)
+                return;
+
+            space += COUNT;
+            PrintTree(root.Right, space);
+
+            for (int i = COUNT; i < space; i++)
+                Console.Write(" ");
+
+            Console.WriteLine(root.Value);
+
+            PrintTree(root.Left, space);
+        }
+        private void PreOrderTraversal(BinaryTreeNode<T> node)
+        {
+            /*
+              PreOrder Traversal visits the current node before its child nodes. 
+              The process for PreOrder Traversal is as follows:
+
+
+                 - Visit the current node.
+                 - Recursively perform PreOrder Traversal of the left subtree.
+                 - Recursively perform PreOrder Traversal of the right subtree.
+            */
+
+            if (node != null)
+            {
+                Console.Write(node.Value + " ");
+                PreOrderTraversal(node.Left);
+                PreOrderTraversal(node.Right);
+            }
+        }
+        public void PreOrderTraversal()
+        {
+            PreOrderTraversal(Root);
+            Console.WriteLine();
+        }
+        private void PostOrderTraversal(BinaryTreeNode<T> node)
+        {
+            /*
+              PostOrder Traversal visits the current node after its child nodes. 
+              The process for PostOrder Traversal is:
+
+
+                - Recursively perform PostOrder Traversal of the left subtree.
+                - Recursively perform PostOrder Traversal of the right subtree.
+                - Visit the current node.
+           */
+
+            if (node != null)
+            {
+                PostOrderTraversal(node.Left);
+                PostOrderTraversal(node.Right);
+                Console.Write(node.Value + " ");
+            }
+        }
+        public void PostOrderTraversal()
+        {
+            PostOrderTraversal(Root);
+            Console.WriteLine();
+        }
+        public void InOrderTraversal()
+        {
+            InOrderTraversal(Root);
+            Console.WriteLine();
+        }
+        private void InOrderTraversal(BinaryTreeNode<T> node)
+        {
+
+            /*
+               Left - Current - Right
+              */
+            if (node != null)
+            {
+                InOrderTraversal(node.Left);
+                Console.Write(node.Value + " ");
+                InOrderTraversal(node.Right);
+            }
+        }
+        public void LevelOrderTraversal()
+        {
+            if (Root == null) return;
+
+
+            Queue<BinaryTreeNode<T>> queue = new Queue<BinaryTreeNode<T>>();
+            queue.Enqueue(Root);
+
+            while (queue.Count > 0)
+            {
+                BinaryTreeNode<T> current = queue.Dequeue();
+                Console.Write(current.Value + " ");
+
+
+                if (current.Left != null)
+                {
+                    queue.Enqueue(current.Left);
+                }
+
+                if (current.Right != null)
+                {
+                    queue.Enqueue(current.Right);
+                }
+            }
         }
     }
 
@@ -68,39 +185,29 @@ namespace GeneralTreeExample
     {
         static void Main(string[] args)
         {
-            // Creating the tree
-            var CompanyTree = new Tree<string>("CEO");
-            var Finance = new TreeNode<string>("CFO");
-            var Tech = new TreeNode<string>("CTO");
-            var Marketing = new TreeNode<string>("CMO");
+            var binaryTree = new BinaryTree<int>();
+            Console.WriteLine("Values to be inserted: 5,3,8,1,4,6,9\n");
 
-            // Adding departments to the CEO node
-            CompanyTree.Root.AddChild(Finance);
-            CompanyTree.Root.AddChild(Tech);
-            CompanyTree.Root.AddChild(Marketing);
+            binaryTree.Insert(5);
+            binaryTree.Insert(3);
+            binaryTree.Insert(8);
+            binaryTree.Insert(1);
+            binaryTree.Insert(4);
+            binaryTree.Insert(6);
+            binaryTree.Insert(9);
 
-            // Adding employees to departments
-            Finance.AddChild(new TreeNode<string>("Accountant"));
-            Tech.AddChild(new TreeNode<string>("Developer"));
-            Tech.AddChild(new TreeNode<string>("UX Designer"));
-            Marketing.AddChild(new TreeNode<string>("Social Media Manager"));
+            binaryTree.PrintTree();
 
-            // Printing the tree
-            CompanyTree.PrintTree();
+            Console.WriteLine("\nPreOrder Traversal (Current-Left SubTree - Right SubTree):");
+            binaryTree.PreOrderTraversal();
 
-            Console.WriteLine("\nFinding Developer...");
-            if (CompanyTree.Find("Developer") == null)
-                Console.WriteLine("Not Found :-(");
-            else
-                Console.WriteLine("Found :-)");
+            Console.WriteLine("\nPostorder Traversal (Left SubTree - Right SubTree - Current):");
+            binaryTree.PostOrderTraversal();
 
-            Console.WriteLine("\nFinding DBA...");
-            if (CompanyTree.Find("DBA") == null)
-                Console.WriteLine("Not Found :-(");
-            else
-                Console.WriteLine("Found :-)");
-
-            Console.ReadKey();
+            Console.WriteLine("\nInorder Traversal: Left-Current-Right");
+            binaryTree.InOrderTraversal();
+            Console.WriteLine("\nLevel-order Traversal");
+            binaryTree.LevelOrderTraversal();
         }
     }
 }
